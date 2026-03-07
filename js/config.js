ae239f92ee771ff1fac953f60c7ac68adbf79699
Priyash <priyash@Mac.lan>
2026-03-07 06:45:01 +0530

    update notion token

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 const AppConfig = {
     geminiKey: 'AIzaSyATTPvFbaXfc2t7PIx3oBqQpRirPIv1HzA',
     geminiModel: 'gemini-2.5-flash',
-    notionToken: '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',
+    notionToken: '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',
     notionDbIdAll: '31853bc5fb4f806985aeddc3992b260e',
     notionDbIdSaved: '31853bc5fb4f809fa4e8c38498805ca8',
-    notionDbIdReports: '31853bc5fb4f806985aeddc3992b260e' // Mapped exactly per user specification
+    notionDbIdReports: '31953bc5fb4f8062a4efc3ad3d99e73c' // Mapped exactly per user specification
 };
